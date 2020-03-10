ad9265d440dfbb61a0d6d98eb42a21588fecf5d8
CBreakr <github email address>
2020-03-10 16:46:27 -0400

    shell for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,46 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set(:views, 'app/views')
 
+  #USERS
+
+  get "/users" do
+    erb :"users/index"
+  end
+
+  get "/users/new" do
+    erb :"users/new"
+  end
+
+  post "/users" do
+
+  end
+
+  get "/users/:id" do
+    erb :"users/show"
+  end
+
+  #RESTAURANTS
+
+  get "/restaurants" do
+    erb :"restaurants/index"
+  end
+
+  get "/restaurants/new" do
+    erb :"restaurants/new"
+  end
+
+  post "/restaurants" do
+
+  end
+
+  get "/restaurants/:id" do
+    erb :"restaurants/show"
+  end
+
+  # I think this makes the most sense
+  # as we are creating a new record
+  post "/restaurants/add_user" do
+
+  end
 
 end
